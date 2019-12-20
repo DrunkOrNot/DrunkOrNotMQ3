24847ed539f894a49cf047add081344fa7863e50
Dominika <ddd.wilk@gmail.com>
2019-12-20 22:18:43 +0100

    Measuring is timeframed. Ugly but works.

--- firebasemq3v2.py
+++ firebasemq3v2.py
@@ -1,30 +1,44 @@
+# This solution needs to be reworked - number of samples that will be taken depend on CPU speed
 import time
 import sys
 from mq3 import *
-#from firebase import firebase
+import statistics
+from datetime import datetime
+# from firebase import firebase
 
-mq = MQ();
+MEASURING_TIME          = 5000           # in miliseconds
+PERC_MIN_VAL            = 0.05
+PERC_MAX_VAL            = 10.0
+SEND_TO_FIREBASE        = True
 
-while True:
-        time.sleep(0.1)
+def measure():
+    mq = MQ();
+    samples = []
+    time_start = int(datetime.timestamp(datetime.now()) * 1000)
+
+    while int(datetime.timestamp(datetime.now()) * 1000) - time_start < MEASURING_TIME:
         try:
-                perc = mq.MQPercentage()
-        except Exception as e :
-                #sys.stdout.write(e)
-                sys.stdout.flush()
-        else:                          
-                #if  0.05 < perc["GAS_ALC"] < 10.0:               
-                        sys.stdout.write("\r")
-                        sys.stdout.write("\033[K")
-                        #sys.stdout.write("Alcohol Detection Level: %g mg/L" % (perc["GAS_ALC"]))
-                        sys.stdout.write("Promiles: %g" % (2.1 * perc["GAS_ALC"]))
-                        sys.stdout.flush()
+            perc = mq.MQPercentage()
+        except Exception as e:
+            print(e)
+        else:
+            if PERC_MIN_VAL < perc["GAS_ALC"] < PERC_MAX_VAL:
+                print("\r")
+                print("\033[K")
+                print("Alcohol Detection Level: %g mg/L" % (perc["GAS_ALC"]))
+                print("Promiles: %g" % (2.1 * perc["GAS_ALC"]))
+                samples.append(perc["GAS_ALC"])
+    return samples
 
-        
-        #firebase = firebase.FirebaseApplication('https://drunkornot-2fb5d.firebaseio.com/')
-        #read_MQ3 = {"ethanol": perc}
-        #firebase.post('/MQ3/', read_MQ3)
+def main():
+    value = statistics.median(measure())
+    print("Finished measuring. Final value: ", value)
 
-def calculate(percentage):
-        return 2.1 * percentage
-        
+    if SEND_TO_FIREBASE:
+        print("Sending to Firebase...")
+        # firebase = firebase.FirebaseApplication('https://drunkornot-2fb5d.firebaseio.com/')
+        # read_MQ3 = {"ethanol": value}
+        # firebase.post('/MQ3/', read_MQ3)
+
+if __name__ == "__main__":
+    main()
